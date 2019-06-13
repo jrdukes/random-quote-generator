74fbd06e1de0d9da77e57c45dd6d0a7de43a34ec
Joe Dukes <joedukes.email@gmail.com>
2019-06-13 11:13:45 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,109 @@
+// Create variable array to store quotes, source, citation, year, and tag
+
+let quotes = [
+  {
+    quote:
+      "Someone's sitting in the shade today because someone planted a tree a long time ago.",
+    source: "Warren Buffett",
+    citation: "Of Permanent Value: The Story of Warren Buffett",
+    year: "2007",
+    tag: "Category: Business"
+  },
+
+  {
+    quote:
+      "Technology is cool, but you've got to use it as opposed to letting it use you.",
+    source: "Prince",
+    citation: "Today",
+    year: "2004",
+    tag: "Category: Technology"
+  },
+
+  {
+    quote:
+      "Information technology is at the core of how you do your business and how your business model itself evolves.",
+    source: "Satya Nadella",
+    tag: "Category: Technology"
+  },
+
+  {
+    quote:
+      "Design is not just what it looks like and feels like. Design is how it works.",
+    source: "Steve Jobs",
+    tag: "Category: Technology"
+  },
+
+  {
+    quote:
+      "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.",
+    source: "Bill Gates",
+    tag: "Category: Technology"
+  }
+];
+
+// Function that pulls a random object from quotes array and store in variable storeRandomQuote
+
+const getRandomQuote = myArray => {
+  var storeRandomQuote;
+  storeRandomQuote = Math.floor(Math.random() * myArray.length);
+  return myArray[storeRandomQuote];
+};
+
+// Function that takes previously selected random quote from array and prints to screen
+
+const printQuote = () => {
+  var getQuotes = getRandomQuote(quotes);
+  var myHtml = "";
+  myHtml = `<p class = "quote">${getQuotes.quote}</p><p class = "source">${
+    getQuotes.source
+  }`;
+
+  // Append year if there is a year:
+  if (getQuotes.year) {
+    myHtml = `${myHtml}<span class='year'> ${getQuotes.year}</span>`;
+  }
+  // Append citation if there is a citation:
+  if (getQuotes.citation) {
+    myHtml = `${myHtml}<span class='citation'> ${getQuotes.citation}</span>`;
+  }
+  // Append tags property if there is one:
+  if (getQuotes.tag) {
+    myHtml = `${myHtml}<br> ${getQuotes.tag}</br>`;
+  }
+  // Append closing p tag:
+  myHtml = `${myHtml}</p>`;
+
+  // Put myHtml on page:
+  document.getElementById("quote-box").innerHTML = myHtml;
+};
+
+// Set interval to refresh page after 5 seconds:
+
+const myInterval = () => setInterval(printQuote, 5000);
+myInterval();
+
+// Event listener respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called:
+
+document
+  .getElementById("loadQuote")
+  .addEventListener("click", printQuote, false);
+
+// Function that generates random rgb value
+
+function randomColorGenerator() {
+  var randomColor;
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
+  randomColor = "rgb(" + red + "," + green + "," + blue + ")";
+  return randomColor;
+}
+
+// Generate random color
+randomColorGenerator();
+
+//Update background with random color
+document.getElementById(
+  "bgcolor"
+).style.backgroundColor = randomColorGenerator();
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Random Quote Generator
+
+Treehouse Project #1 - Random Quote Generator
+
+# Project Requirements
+
+## Create an array of JS objects to hold the data for the quotes.
+
+Each quote should have the following properties:
+
+- [x] A `quote` property which contains a string: the text of the quote to display on the page.
+- [x] A `source` property which contains a string identifying the creator of the quote.
+- [x] An optional `citation` property which contains a string identifying the publication the quote appears in.
+- [x] An optional `year` property which contains a number identifying the date of the quote.
+
+## Create a function named `getRandomQuote` which:
+
+- [x] selects a random quote object from the quotes array
+- [x] returns the randomly selected quote objects
+
+## Create a function named `printQuote` which follows these rules:
+
+- [x] `printQuote` calls the `getRandomQuote` function and stores the returned quote object in a variable.
+
+- [x] `printQuote` doesn't add a `<span class="citation">` for a missing citation or a `<span class="year">` if the year property is missing.
+- [x] `printQuote` displays the final HTML string to the page. You can use the following JS snippet to accomplish that: `document.getElementById('quote-box').innerHTML`
+
+# Extra Credit
+
+- [x] Randomly change the background color of the page, when the quote changes
+- [x] Refresh the quote after a set amount of time. For example, every 30 seconds, make a new quote appear. (You can use the setInterval() or setTimeout() method to do this -- see the links in the Project Resources listing.)
